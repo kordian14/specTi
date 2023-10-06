78320bae5511d0c2373624c28b222ee78f62ea31
kordian14 <144421175+kordian14@users.noreply.github.com>
2023-10-06 09:26:38 +0200

    Add files via upload

---
+++ kredyt_2/app/calc.php
@@ -0,0 +1,77 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+
+
+
+//Pobieranie danych
+function getParams(&$kwota,&$procent,&$rata){
+	$kwota = isset($_REQUEST['kwota']) ? $_REQUEST['kwota'] : null;
+	$procent = isset($_REQUEST['procent']) ? $_REQUEST['procent'] : null;
+	$rata = isset($_REQUEST['rata']) ? $_REQUEST['rata'] : null;	
+}
+
+
+
+//walidacja parametrów
+function validate(&$kwota,&$procent,&$rata,&$messages){
+	// sprawdzenie, czy parametry zostały przekazane
+	if ( ! (isset($kwota) && isset($procent) && isset($rata))) {
+		return false;
+	}
+
+	// sprawdzenie, czy podano wartości
+	if ( $kwota == "") {
+		$messages [] = 'Nie podano kwoty kredytu';
+	}
+	if ( $procent == "") {
+		$messages [] = 'Nie podano oprocentowania kredytu';
+	}
+	if ( $rata == "") {
+		$messages [] = 'Nie podano ilości rat kredytu';
+	}
+
+	if (count ( $messages ) != 0) return false;
+	
+	//sprawdzenie czy zmienne są liczbami
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą';
+	}
+	
+	if (! is_numeric( $procent )) {
+		$messages [] = 'Druga wartość nie jest liczbą';
+	}	
+
+	if (! is_numeric( $rata )) {
+		$messages [] = 'Druga wartość nie jest liczbą';
+	}	
+
+
+	if (count ( $messages ) != 0) return false;
+	else return true;
+}
+
+//funkcja licząca ratę
+function process(&$kwota,&$procent,&$rata,&$messages,&$result){
+	$kwota = intval($kwota);
+	$procent = floatval($procent); //Procent może być zmiennoprzecinkowy
+	$rata = intval($rata);
+
+	//wykonanie operacji
+	$result= (( $procent / 100) * $kwota + $kwota) / $rata;
+	$result = round($result, 2); // zaokrąglenie może być zmienno przecinkowe
+
+}
+ 
+//zaczyna się kontroler
+$kwota = null;
+$procent = null;
+$rata = null;
+$result = null;
+$messages = array();
+getParams($kwota, $procent, $rata);
+	if ( validate($kwota,$procent,$rata,$messages) ){
+		process($kwota,$procent,$rata,$messages,$result);
+	}
+
+include 'kredyt_widok.php';
