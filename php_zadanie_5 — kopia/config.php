3c861191a70a9bc642eece9f80e577d42a53acce
kordian14 <144421175+kordian14@users.noreply.github.com>
2023-11-17 11:18:10 +0100

    Add files via upload

---
+++ php_zadanie_5 — kopia/config.php
@@ -0,0 +1,17 @@
+<?php
+//Aby aplikacja działała poprawnie folder główny musi mieć nazwę "kredyt_2"
+define('_SERVER_NAME', 'localhost:80');
+define('_SERVER_URL', 'http://'._SERVER_NAME);
+define('_APP_ROOT', '/php_zadanie_5');
+define('_APP_URL', _SERVER_URL._APP_ROOT);
+define("_ROOT_PATH", dirname(__FILE__));
+
+/*function out(&$param){
+	if (isset($param)){
+		echo $param;
+	}
+}
+Nie jest potrzebne
+*/
+
+?>
---
+++ php_zadanie_5 — kopia/templates/header.php
@@ -0,0 +1,14 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?php print(_APP_URL); ?>/css/styl.css">
+
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator raty kredytu</title>
+</head>
+<header>
+Obliczanie raty kredytu
+
+</header>
